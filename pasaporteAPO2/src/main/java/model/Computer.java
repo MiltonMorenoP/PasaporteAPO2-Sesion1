4b07e8219f2b568985b3a104f14aaab37a4c6297
MiltonMorenoP <andresito242008@gmail.com>
2025-12-03 10:58:43 -0500

    feature: add Computer class

--- pasaporteAPO2/src/main/java/model/Computer.java
+++ pasaporteAPO2/src/main/java/model/Computer.java
@@ -1,4 +1,30 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Computer {
+
+    private String serialNumber;
+    private ArrayList<Incident> localIncidents;
+
+    public Computer(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public ArrayList<Incident> getLocalIncidents() {
+        return localIncidents;
+    }
+
+    public void setLocalIncidents(ArrayList<Incident> localIncidents) {
+        this.localIncidents = localIncidents;
+    }
+    
 }
